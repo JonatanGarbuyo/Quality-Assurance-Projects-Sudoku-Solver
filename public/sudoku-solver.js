971d9766099b0c11c4c55386ccf2a657a3379735
Glitch (arrow-certain-side) <none>
2020-07-03 20:05:48 +0000

    🐴🍕 Checkpoint ./public/sudoku-solver.js:17402204/224

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -19,7 +19,7 @@ const fillGrid = (values) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
-  textArea.value = ""//'..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
   console.log(textArea.value);////////////////////////
   //fillGrid(textArea.value); 
   
