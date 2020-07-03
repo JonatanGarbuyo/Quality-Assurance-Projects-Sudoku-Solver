4cc2038b4e740045c3c7ac2ddc77a7f93ffa9202
Glitch (arrow-certain-side) <none>
2020-07-03 18:21:12 +0000

    🍍🎋 Checkpoint ./public/sudoku-solver.js:17402204/9

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
   textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 });
 
+
+
+
+
+
+
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
@@ -13,6 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 */
 try {
   module.exports = {
-  
+    
   }
 } catch (e) {}
