da97374d62d55c50b6dbc66d93d299608c2247ce
Glitch (arrow-certain-side) <none>
2020-07-03 19:06:42 +0000

    👫🔑 Checkpoint ./public/sudoku-solver.js:17402204/68

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,16 +1,27 @@
 const textArea = document.getElementById('text-input');
 // import { puzzlesAndSolutions } from './puzzle-strings.js';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listen for changes in the textArea
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
   textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 });
-
-
-
-
-
-
 
 
 /* 
