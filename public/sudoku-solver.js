08a8ac6e30b9300340091b9ff61a97d6fd1896b6
Glitch (arrow-certain-side) <none>
2020-07-03 19:19:40 +0000

    ⛈🕹 Checkpoint ./public/sudoku-solver.js:17402204/280

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,15 +1,10 @@
+const grid = document.getElementById('text-input');
 const textArea = document.getElementById('text-input');
 // import { puzzlesAndSolutions } from './puzzle-strings.js';
 
-
-
-
-
-
-
-
-
-
+const fillGrid = (values) => {
+  
+};
 
 
 
@@ -21,6 +16,9 @@ const textArea = document.getElementById('text-input');
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
   textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  
+  fillGrid(textArea.value);
+  
 });
 
 
