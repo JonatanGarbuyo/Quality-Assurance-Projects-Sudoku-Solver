1bc24a6d844abf0948d904c31aaad027346e7731
Glitch (arrow-certain-side) <none>
2020-07-03 22:48:21 +0000

    🌒🛎 Checkpoint ./public/sudoku-solver.js:17402204/782 ./views/index.html:17402204/102

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -19,15 +19,20 @@ document.addEventListener("DOMContentLoaded", event => {
     "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
   //console.log(textArea.value);////////////////////////
   fillGrid(textArea.value);
+  
+  
+  // listen for changes in the textArea
+  textArea.addEventListener('input', (e)=> console.log(e.data));
+  
 });
 
-  // listen for changes in the textArea
-  textArea.addEventListener("input", fillGrid(this.value));
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
 try {
-  module.exports = {};
+  module.exports = {
+    fillGrid
+  };
 } catch (e) {}
